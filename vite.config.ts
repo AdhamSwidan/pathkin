f9592618a9103f857d74ebb03ef95f7b6b551301
Adham Swidan <adham.swidan@gmail.com>
2025-10-01 17:30:53 +0200

    Fix environment variables handling for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/wanderlodge-project/',
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY)
